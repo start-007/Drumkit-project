7528010906a8864dc069b91c14c46442335414c5
Teja <starteja.007@gmail.com>
2021-12-24 22:49:31 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,66 @@
+var drum=document.querySelectorAll(".drum");
+for(var i=0;i<drum.length;++i){
+    drum[i].addEventListener("click",function(){
+        var btninnerhtml=this.innerHTML;
+        makeSound(btninnerhtml);
+        btnAnimation(btninnerhtml)
+    });
+    
+}
+
+document.addEventListener("keypress",function(event){
+    makeSound(event.key);
+    btnAnimation(event.key);
+});
+
+function makeSound(key){
+   
+    switch (key){
+        case "w":
+                var tom1=new Audio("sounds/tom-1.mp3");
+                tom1.play();
+                break;
+        case "a":
+                var tom2=new Audio("sounds/tom-2.mp3");
+                tom2.play();
+                break;
+        case "s":
+                var tom3=new Audio("sounds/tom-3.mp3");
+                tom3.play();
+                break;
+        case "d":
+                var tom4=new Audio("sounds/tom-4.mp3");
+                tom4.play();
+                break;
+        case "j":
+                var crash=new Audio("sounds/crash.mp3");
+                crash.play();
+                break;
+        case "k":
+                var kickBass=new Audio("sounds/kick-bass.mp3");
+                kickBass.play();
+                break;
+        case "l":
+                var snare=new Audio("sounds/snare.mp3");
+                snare.play();
+                break;
+        default:
+                console.log(drumInnerhtml);
+        
+    }
+}
+
+
+function btnAnimation(key){
+    
+    var activeVar=document.querySelector("."+key);
+    activeVar.classList.toggle("pressed");
+    setTimeout(function(){
+        activeVar.classList.remove("pressed");
+    },100);
+    
+}
+
+
+
+
